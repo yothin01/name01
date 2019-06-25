4cbd8e3b829eb4e347679a41a4ba4059c5426711
yothin01 <52157175+yothin01@users.noreply.github.com>
2019-06-25 22:06:24 +0700

    Create name1.cpp

---
+++ name1.cpp
@@ -0,0 +1,24 @@
+/* program 25/06/2562 Yothin kaewmongkol */
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	string name1 = "Yothin Kaewmongkol";//ชื่อครับ//
+	int  score1 = 80 ,score2 = 70;//คะเเนน//
+	char a = 'A';//เกรด//
+	double b = 3.99 ;//เกรด//
+	cout<<"myname is : "<< name1 << endl;
+	cout<<"score1 : "<<  score1 << endl;
+	cout<<"score2 : "<<  score2 << endl;
+	cout<<"total : " << score1+score2 << endl;
+	cout<<"Grade : " << b<<endl;
+	cout<<"Grade : "<< a << endl;
+	return(0);
+
+
+}
+        
+
+
+
